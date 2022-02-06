932d83cb7eb4158749c0640b99dcafbe5523d80c
Divya251199 <92331091+Divya251199@users.noreply.github.com>
2022-02-06 19:11:44 +0530

    Add files via upload

---
+++ Assignments/Jan-31/index.js
@@ -0,0 +1,27 @@
+console.time('hiee')
+console.log('alert')
+// console.clear()
+console.log('Di')
+console.log(5+7)
+console.log(4*9)
+console.log('Divya');
+console.log('tom');
+console.log(8*9);
+console.log(15/12);
+console.timeEnd('hi')
+console.clear()
+let booleanvar=true
+console.log(booleanvar,(typeof booleanvar))
+let arr=[1,2,3,4,5,6,7,8,9]
+console.log(arr,(typeof arr))
+console.log(7*8)
+console.clear()
+    console.log('we are tut 6')
+const name="Divya"
+const greeting='good morning '
+console.log(greeting+name)
+let html;
+html="<h1> this is heading</h1>",
+      "<p> this is my para </p>";
+
+    console.log(html.indexOf('h1'));
